cd930060fda232edfca27bef10c7679f4506d674
Vu Chuc <vuchuc781999@gmail.com>
2025-04-15 08:49:26 +0100

    15/04/2025

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,11 +6,11 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-vim.api.nvim_create_autocmd("BufDelete", {
-  callback = function()
-    local bufs = vim.t.bufs
-    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
-      vim.cmd "Nvdash"
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufDelete", {
+--   callback = function()
+--     local bufs = vim.t.bufs
+--     if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+--       vim.cmd "Nvdash"
+--     end
+--   end,
+-- })
