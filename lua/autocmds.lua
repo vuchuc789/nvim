b5f1601f0e3b925b1f7d7cd1d0c94d11b4000f00
Vu Chuc <vuchuc781999@gmail.com>
2025-04-18 19:19:36 +0100

    18/04/2025

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,10 +1,10 @@
 -- auto close location lists
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "qf",
-  callback = function()
-    vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CR>:cclose<CR>", { noremap = true, silent = true })
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "qf",
+--   callback = function()
+--     vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CR>:cclose<CR>", { noremap = true, silent = true })
+--   end,
+-- })
 
 -- vim.api.nvim_create_autocmd("BufDelete", {
 --   callback = function()
