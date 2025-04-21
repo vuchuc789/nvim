4dc9ba968dd9472851990f7a01fdeae38a757d38
Vu Chuc <vuchuc781999@gmail.com>
2025-04-21 09:21:32 +0100

    21/04/2025

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -281,4 +281,28 @@ return {
       },
     },
   },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "hrsh7th/cmp-cmdline",
+        event = "CmdlineEnter",
+        config = function()
+          local cmp = require "cmp"
+
+          cmp.setup.cmdline("/", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = { { name = "buffer" } },
+          })
+
+          cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+            matching = { disallow_symbol_nonprefix_matching = false },
+          })
+        end,
+      },
+    },
+  },
 }
